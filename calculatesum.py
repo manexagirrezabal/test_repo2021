272b901378be3d2c55898c82b1c31f6762ea8542
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2021-12-17 15:58:09 +0100

    Added danish

--- calculatesum.py
+++ calculatesum.py
@@ -5,5 +5,6 @@ number1 = int(sys.argv[1])
 number2 = int(sys.argv[2])
 
 
-print ("The sum of the two numbers that you gave me is: ",end="")
+print ("The sum of the two numbers that you gave me is: ")
+print ("Summen af de to tal du gav mig er: ")
 print (number1 + number2)
